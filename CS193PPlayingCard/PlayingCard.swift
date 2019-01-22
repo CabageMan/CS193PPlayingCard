15c119fc894b45c9c76a6f8e1c96c0241266f352
ViktorsMacbook <viktorsmacbook@ViktorsMacbooks-MacBook-Pro.local>
2019-01-22 20:21:59 +0300

    Create PlayingCards model

---
+++ CS193PPlayingCard/PlayingCard.swift
@@ -0,0 +1,39 @@
+//
+//  PlayingCard.swift
+//  CS193PPlayingCard
+//
+//  Created by ViktorsMacbook on 22.01.19.
+//  Copyright © 2019 Viktor Bednyi Inc. All rights reserved.
+//
+
+import Foundation
+
+struct PlaingCard {
+    
+    
+    
+    enum Suit: String {
+        case spades: "♠️"
+        case hearts: "♥️"
+        case clubs: "♣️"
+        case diamonds: "♦️"
+    }
+    
+    enum Rank {
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+                case .ace: return 1
+                case .numeric(let pips): pips
+                case .face(let kind): where kind == "J" return 11
+                case .face(let kind): where kind == "Q" return 12
+                case .face(let kind): where kind == "K" return 13
+                default: return 0
+            }
+        }
+    }
+    
+}
